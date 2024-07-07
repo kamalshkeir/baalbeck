6d7bfdee4678d8ee1bb640075cc30a2511ff7764
kamal shkeir <kamalshkeir@gmail.com>
2024-07-07 13:49:10 +0200

    add module baalbeck

---
+++ b.go
@@ -0,0 +1,9 @@
+package baalbeck
+
+func AddOne(i int) int {
+	return i + 1
+}
+
+func AddTwo(i int) int {
+	return i + 2
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module test
+module github.com/kamalshkeir/baalbeck
 
 go 1.22.3
